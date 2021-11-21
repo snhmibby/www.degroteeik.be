49c64ba6b106658cd6698a3ae9be3af71c79a1b5
Jur <jurjenm@yahoo.com>
2021-11-21 23:33:35 +0100

    more fidgetting with gallery

--- assets/components/gallery/gallery.js
+++ assets/components/gallery/gallery.js
@@ -20,15 +20,15 @@ class Gallery {
 		let nelem = this.items.length
 		for (let i = 0; i < this.items.length; i++) {
 			let item = this.items[i]
-			let thumb = item.querySelector('.gallery-thumb')
-			let modal = item.querySelector('.gallery-modal')
-			let full = item.querySelector('.gallery-full')
 
 			//install next/prev buttons around the full-size image
+			let full = item.querySelector('.gallery-full')
 			this.addButton(full, (i - 1 + nelem) % nelem, "<", 'beforebegin')
 			this.addButton(full, (i + 1) % nelem, ">", 'afterend')
 
 			//click handlers hide/show modal
+			let thumb = item.querySelector('.gallery-thumb')
+			let modal = item.querySelector('.gallery-modal')
 			thumb.onclick = () => this.show(modal)
 			modal.onclick = () => this.hide(modal)
 		}
